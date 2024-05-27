b212e2503ee16c41e415eb3829bce437718f75f6
ajithamin004 <ajithsowda33@gmail.com>
2024-05-28 00:21:16 +0530

    adds mongodb connections

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-ÿþ
+import "dotenv/config";
+import dbConnection from './db/index.js'
+import { app } from './app.js';
+
+const PORT = process.env.PORT || 8000;
+
+try{
+await dbConnection();
+app.listen(PORT,()=>{
+    console.log('listning on port', PORT)
+})
+}catch(err){
+    console.log('error occured while connection')
+}
